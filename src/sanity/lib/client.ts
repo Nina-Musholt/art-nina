472fe091c827e432ac666251e8c8f8801b59d4fa
Nina-Musholt <nina_musholt@yahoo.de>
2025-08-17 22:00:23 +0200

    Update: artwork schema and other fixes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,6 +2,13 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
+// 🔎 Debug: print env vars during build
+console.log("SANITY ENV CHECK", {
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+});
+
 // Setup Sanity client
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
